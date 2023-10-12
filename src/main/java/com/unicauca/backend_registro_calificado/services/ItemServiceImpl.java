fe01339cd53e6574d0848c2e4c2bc566cfc60950
Alvaro Erazo <ader148@hotmail.com>
2023-10-12 01:44:51 -0500

    se agrego endpoints de item y subitem v2

--- src/main/java/com/unicauca/backend_registro_calificado/services/ItemServiceImpl.java
+++ src/main/java/com/unicauca/backend_registro_calificado/services/ItemServiceImpl.java
@@ -2,15 +2,41 @@ package com.unicauca.backend_registro_calificado.services;
 
 import com.unicauca.backend_registro_calificado.domain.ItemDTO;
 import com.unicauca.backend_registro_calificado.domain.Response;
+import com.unicauca.backend_registro_calificado.model.Item;
+import com.unicauca.backend_registro_calificado.repository.IitemRepository;
 import org.springframework.stereotype.Service;
-import com.unicauca.backend_registro_calificado.services.IitemService;
+import org.modelmapper.ModelMapper;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ItemServiceImpl implements IitemService{
+
+    private final IitemRepository iitemRepository;
+
+    private final ModelMapper modelMapper;
+
+    public ItemServiceImpl(ModelMapper modelMapper, IitemRepository iitemRepository) {
+        this.iitemRepository = iitemRepository;
+        this.modelMapper = modelMapper;
+    }
+
+
     @Override
     public Response<List<ItemDTO>> findAllItemsByIdRegistroCalificado(String idRegCalificado) {
         return null;
     }
+
+    @Override
+    //public Response<ItemDTO> findItemById(String IdItem){
+    public ItemDTO findItemById(String IdItem){
+        System.out.println("IdItem: " + IdItem);
+        Optional<Item> item = this.iitemRepository.findById(IdItem);
+        System.out.println("IdItem itemid: " + item.get().getId());
+        return modelMapper.map(this.iitemRepository.findById(IdItem), ItemDTO.class);
+        //return null;
+    };
+
+
 }
--- src/main/java/com/unicauca/backend_registro_calificado/util/mapper/MapperBean.java
+++ src/main/java/com/unicauca/backend_registro_calificado/util/mapper/MapperBean.java
@@ -1,2 +1,14 @@
-package com.unicauca.backend_registro_calificado.util.mapper;public class MapperBean {
+package com.unicauca.backend_registro_calificado.util.mapper;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.modelmapper.ModelMapper;
+
+@Configuration
+public class MapperBean {
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+
 }
